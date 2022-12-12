ff84b8ad3c1c3c7a8b8df6632c271190cf1e0a6b
VCLdesigns <Bart.Victor.Loots@gmail.com>
2022-12-12 11:16:10 +0100

    worked in homepage nightbox

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ var navIconOpened = document.querySelector("#nav_icon_openend");
 var navMenu = document.querySelector("#nav_links");
 var deNacht = document.querySelector("#nacht_kader");
 var deSterren = document.querySelectorAll(".sterren");
+var deHuisLichten = document.querySelector("#mansion_lights_house");
+var deTorenLichten = document.querySelector("#mansion_lights_tower");0
 
 navIconClosed.addEventListener("click", menuToggle);
 navIconOpened.addEventListener("click", menuToggle);
@@ -18,7 +20,13 @@ for (var i = 0; i < deSterren.length; i++) {
     var randomLeft = Math.random() * 100;
     // top moet van 0px tot 500px gaan
     var randomTop = Math.random() * 500
+    var randomWidthHeight = Math.random() * 3 + 1;
     //verspreiding van de sterren
+    deSterren[i].style.width = randomWidthHeight + "px";
+    deSterren[i].style.height = randomWidthHeight + "px";
     deSterren[i].style.left = randomLeft + "%";
     deSterren[i].style.top = randomTop + "px";
+    deSterren[i].style.animation = "knipperen 5s ease-in infinite";
+    deSterren[i].style.animationDelay = (i - 100) + "s";
 }
+
